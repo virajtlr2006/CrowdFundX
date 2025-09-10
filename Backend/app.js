50cceec01ae2db9d3671ec78791edac3a6715df5
Yash Parekh <parekhyash1103@gmail.comm>
2025-09-10 12:35:07 +0530

    set up done

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,21 @@
-Darji
+// require all 
+
+const express = require("express");
+const cors = require("cors");
+const app = express()
+// middleware
+app.use(express.json())
+
+app.use(cors())
+
+app.listen(8080,async () => {
+    console.log("Welcome to crowdfund");
+    
+})
+// root route
+app.get("/",async (req,res) => {
+    res.json({
+        "msg":"root working"
+    })
+    
+})
